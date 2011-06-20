10c0a462e13a93d319a538d4eacd73d1c571b09c
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-20 07:53:47 +0000

    first setup of the website
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inspectr@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,7 +43,9 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p> <strong>inspectr</strong> - A R package for storing and manipulating Vis-NIR data </p>
+
+<img alt="inspectr" src="img/inspectr.png">
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
