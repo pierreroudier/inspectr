2e8aff0d0a4f67ff839d63287098ff9391d71255
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-10 04:11:46 +0000

    Major changes including:
    * Switch the get_data()/data() methods to features() (as data() is protected)
    * Introduced sp bindings - and thus new spatial aware classes
    * Changed classes definitions location in a single file
    
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inspectr@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/inspectr/R/AAA-Classes.R
@@ -0,0 +1,129 @@
+#' Class for spectra collections
+#'
+#' @slot wl object of class "\code{numeric}"; the wavelengths at which the spectra has been measured
+#' @slot nir object of class "\code{matrix}"; the spectra, with as many columns as wavelengths, and as many rows as samples
+#' @slot id object of class "\code{data.frame}" with one attribute; the identification strings for each sample in the collection
+#' @slot units object of class "\code{character}"; units in which the wavelengths are expressed
+#' @seealso \code{\link{spectra}}, \code{\link{wl}}, \code{\link{SpectraDataFrame-class}}
+#' @author Pierre Roudier \email{pierre.roudier@@gmail.com}
+#' @rdname Spectra-class
+#' @exportClass Spectra
+setClass(
+  Class="Spectra",
+  representation=representation(
+    wl = 'numeric',
+    nir = 'matrix',
+    id = 'data.frame',
+    units = "character"
+  ),
+  prototype = prototype(
+    wl = numeric(),
+    nir = matrix(),
+    id = data.frame(NA),
+    units = as.character(NA)
+  ),
+  validity = function(object) {
+    # if the wl are given as an integer vector they are translated into a numeric vector
+    # for clarity (only one type to manage)
+    if (is(object@wl, "integer"))
+      object@wl <- as.numeric(object@wl)
+    if (!inherits(object@wl, "numeric"))
+      stop("wl should be of class integer or numeric")
+    if ((nrow(object@id) > 0) & (nrow(object@nir) != nrow(object@id)))
+      stop("number of individuals and number of rows in the spectra matrix don't match")
+    if ((length(object@wl > 1) & (ncol(object@nir) != length(object@wl))))
+      stop("number of columns in the spectra matrix and number of observed wavelengths don't match")
+    if (length(unique(object@id[,1])) != nrow(object@id))
+      stop("The ids of the samples need to be unique")
+    return(TRUE)
+  }
+)
+
+#' Class for spectra collections with associated attributes
+#'
+#' @slot wl object of class "\code{numeric}"; the wavelengths at which the spectra has been measured
+#' @slot nir object of class "\code{matrix}"; the spectra, with as many columns as wavelengths, and as many rows as samples
+#' @slot id object of class "\code{data.frame}" with one attribute; the identification strings for each sample in the collection
+#' @slot units object of class "\code{character}"; units in which the wavelengths are expressed
+#' @slot data object of class data.frame containing the attribute data
+#' @seealso \code{\link{spectra}}, \code{\link{wl}}, \code{\link{Spectra-class}}
+#' @author Pierre Roudier \email{pierre.roudier@@gmail.com}
+#' @rdname SpectraDataFrame-class
+#' @exportClass SpectraDataFrame
+setClass(
+  Class='SpectraDataFrame',
+  representation=representation(
+    "Spectra",
+    data='data.frame'
+  ),
+  prototype=prototype(
+    wl = numeric(),
+    nir = matrix(),
+    id = data.frame(NA),
+    units = as.character(NA),
+    data = data.frame()
+  ),
+  validity = function(object) {
+    if (ncol(object@data) == 0)
+      stop("data.frame is empty: use Spectra() to create spectra-only object")
+    if (nrow(object@data) != nrow(object@nir))
+      stop("number of rows in data.frame and spectra don't match")
+    return(TRUE)
+  }
+)
+
+#' Class for spectra collections that have
+#' associated spatial point locations
+#'
+setClass(
+  Class='SpatialSpectra',
+  representation=representation(
+    'SpatialPoints',
+    'Spectra'
+  ),
+  prototype=prototype(
+    bbox = matrix(NA),
+    proj4string = CRS(as.character(NA)),
+    coords = matrix(NA),
+    wl = numeric(),
+    nir = matrix(),
+    id = data.frame(NA),
+    units = as.character(NA)
+  )
+)
+
+#' Class for spectra collections that have
+#' associated attributes and spatial point locations
+#'
+#' @slot wl object of class "\code{numeric}"; the wavelengths at which the spectra has been measured
+#' @slot nir object of class "\code{matrix}"; the spectra, with as many columns as wavelengths, and as many rows as samples
+#' @slot id object of class "\code{data.frame}" with one attribute; the identification strings for each sample in the collection
+#' @slot units object of class "\code{character}"; units in which the wavelengths are expressed
+#' @slot data object of class data.frame containing the attribute data (may or may not contain the coordinates in its columns)
+#' @slot bbox object of class "matrix"; bounding box
+#' @slot proj4string object of class "CRS"; projection string
+#' @slot coords object of class "matrix"; the coordinates matrix (points are rows in the matrix)
+#' @slot coords.nrs object of class logical; if TRUE, when the object was created the coordinates were retrieved from the data.frame, and hence stripped from it; after coercion to data.frame, e.g. by as.data.frame(x), coordinates will again be added (as ﬁrst few columns) to the data.frame
+#' @seealso \code{\link{spectra}}, \code{\link{wl}}, \code{\link{coordinates}}, \code{\link{SpectraDataFrame-class}}
+#' @author Pierre Roudier \email{pierre.roudier@@gmail.com}
+#' @rdname SpatialSpectraDataFrame-class
+#' @exportClass SpatialSpectraDataFrame
+#' @import sp
+setClass(
+  Class = 'SpatialSpectraDataFrame',
+  representation = representation(
+    'SpatialPointsDataFrame',
+    'SpectraDataFrame'
+  ),
+  prototype = prototype(
+    bbox = matrix(NA),
+    proj4string = CRS(as.character(NA)),
+    coords = matrix(NA),
+    coords.nrs = numeric(0),
+    wl = numeric(),
+    nir = matrix(),
+    id = data.frame(NA),
+    units = as.character(NA),
+    data = data.frame()
+  )
+)
---
+++ pkg/inspectr/R/SpatialSpectraDataFrame-methods.R
@@ -0,0 +1,115 @@
+#' @include SpectraDataFrame-methods.R
+# roxygen()
+
+"SpatialSpectraDataFrame" <- function(coords, nir, data,
+  coords.nrs = numeric(0), proj4string = CRS(as.character(NA)), match.ID = TRUE, bbox=NULL,
+  wl=numeric(), id=data.frame(id=NA), units=as.character(NA)) {
+
+  if (!is(coords, "SpatialPoints"))
+    coords = SpatialPoints(coords, proj4string = proj4string, bbox=bbox)
+
+  if (!is(nir, "Spectra"))
+    nir = Spectra(wl=wl, nir=nir, id=id, units=units)
+
+  new("SpatialSpectraDataFrame", coords, nir, data=data)
+}
+
+## coordinates<- setter
+##
+## Adapted from sp package code
+##
+setReplaceMethod("coordinates", signature(object = "SpectraDataFrame", value = "ANY"),
+  function(object, value) {
+    coord.numbers = NULL
+    data <- features(object)
+
+    if (inherits(value, "formula")) {
+      cc = model.frame(value, object) # retrieve coords
+      if (dim(cc)[2] == 2) {
+	nm = as.character(as.list(value)[[2]])[2:3]
+	coord.numbers = match(nm, names(data))
+      }
+      else if (dim(cc)[2] == 3) {
+	nm = c(as.character(as.list((as.list(value)[[2]])[2])[[1]])[2:3], as.character(as.list(value)[[2]])[3])
+	coord.numbers = match(nm, names(data))
+      } # else: give up.
+    }
+
+    else if (is.character(value)) {
+      cc = data[, value] # retrieve coords
+      coord.numbers = match(value, names(data))
+    }
+
+    else if (is.null(dim(value)) && length(value) > 1) { # coord.columns?
+      if (any(value != as.integer(value) || any(value < 1)))
+	stop("coordinate columns should be positive integers")
+
+      cc = data[, value] # retrieve coords
+      coord.numbers = value
+    }
+
+    else  # raw coordinates given; try transform them to matrix:
+      cc = coordinates(value)
+
+    if (any(is.na(cc)))
+      stop("coordinates are not allowed to contain missing values")
+
+    if (!is.null(coord.numbers)) {
+      data = data[ , -coord.numbers, drop = FALSE]
+      stripped = coord.numbers
+      # ... but as.data.frame(x) will merge them back in, so nothing gets lost.
+      if (ncol(data) == 0)
+	#stop("only coords columns present: use SpatialSpectra to create a points object without data")
+	return(SpatialSpectra(cc))
+    }
+    else
+      stripped = numeric(0)
+
+    SpatialSpectraDataFrame(coords = cc, data = data, coords.nrs = stripped, object, match.ID = FALSE)
+  }
+)
+
+## coercition methods
+
+as.data.frame.SpatialSpectraDataFrame = function(x, ...)  {
+  coords <- as.data.frame(as(x, 'SpatialPoints'))
+  nir <- as.data.frame(spectra(x))
+  data <- features(x)
+  id <- id(x)
+  df <- data.frame(id, coords, data, nir)
+  names(df) <- c('id', coordnames(x), names(data), wl(x))
+  df
+}
+
+setAs("SpatialSpectraDataFrame", "data.frame", function(from)
+	as.data.frame.SpatialSpectraDataFrame(from))
+
+## data manipulation
+subset.SpatialSpectraDataFrame <- function(x, subset, select, drop = FALSE, ...) {
+  # adapted from subset.data.frame
+  df <- features(x)
+  if (missing(subset))
+        r <- TRUE
+  else {
+    e <- substitute(subset)
+    r <- eval(e, df, parent.frame())
+    if (!is.logical(r))
+	stop("'subset' must evaluate to logical")
+    r <- r & !is.na(r)
+  }
+  if (missing(select))
+    vars <- TRUE
+  else {
+    nl <- as.list(seq_along(df))
+    names(nl) <- names(df)
+    vars <- eval(substitute(select), nl, parent.frame())
+  }
+  df_sub <- df[r, vars, drop = drop]
+  SpPts <- as(x, 'SpatialPoints')
+  SpPts <- SpPts[r, ]
+  # remove unused factors
+  df_sub <- droplevels(df_sub)
+  id_selected <- which(rownames(df) %in% rownames(df_sub))
+  x <- SpatialSpectraDataFrame(SpPts, wl = wl(x), nir = spectra(x)[id_selected, , drop = FALSE], id = id(x)[id_selected, 1, drop = FALSE], units = get_units(x), data = df_sub)
+  x
+}
