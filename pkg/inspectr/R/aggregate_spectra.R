73912eaf985428e80d404cfb11745587941bb732
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-10 04:09:21 +0000

    further improvements to the aggregation stuff
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inspectr@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inspectr/R/aggregate_spectra.R
+++ pkg/inspectr/R/aggregate_spectra.R
@@ -7,6 +7,7 @@
 #'
 #' @param obj an object inheriting from class Spectra
 #' @param fun an aggregation function
+#' @param id an aggregation identifier
 #' @param ... expressions evaluated in the context of \code{fun}
 #' @return An object of the same class as \code{obj}
 #' @export
@@ -18,20 +19,50 @@ if (!isGeneric("aggregate_spectra"))
     standardGeneric("aggregate_spectra"))
 
 setMethod("aggregate_spectra", "Spectra",
-  function(obj, fun = mean, ...){
+  function(obj, fun = mean, id = NULL, ...){
     # making up an id name from the aggregation function
-    id <- as.character(substitute(fun))[1]
+    new_id <- as.character(substitute(fun))[1]
 
     # applying the function to the spectra
     nir <- aaply(.data = spectra(obj), .margins = 2, .fun = fun, ...)
 
-    res <- Spectra(wl=wl(obj), nir=nir, id=id, units=units(obj))
+    res <- Spectra(wl = wl(obj), nir = nir, id = new_id, units = units(obj))
 
-    # if there is some data
-    if ("data" %in% slotNames(obj)) {
-#     if (is(obj, 'SpectraDataFrame')) {
+    res
+  }
+)
+
+setMethod("aggregate_spectra", "SpectraDataFrame",
+  function(obj, fun = mean, id = NULL, ...){
+
+    if (is.null(id)) {
+      # making up an id name from the aggregation function
+      new_id <- as.character(substitute(fun))[1]
+      # applying the function to the spectra
+      nir <- aaply(.data = spectra(obj), .margins = 2, .fun = fun, ...)
+      res <- Spectra(wl = wl(obj), nir = nir, id = new_id, units = units(obj))
       data <- aaply(.data = data(obj), .margins = 2, .fun = fun, ...)
-      res <- SpectraDataFrame(res, data=data.frame(matrix(data, nrow=1, dimnames=list(id, names(data)))))
+      res <- SpectraDataFrame(res, data = data.frame(matrix(data, nrow = 1, dimnames = list(id, names(data)))))
+    }
+
+    else {
+      if (id %in% names(data(obj))) {
+
+        idx <- which(names(data(obj)) == id)
+        s <- data.frame(id = data(obj)[, idx, drop = FALSE], spectra(obj))
+        s <- dlply(s, id, mean, ...)
+        s <- do.call("rbind", s)
+        s[, 1] <- rownames(s)
+
+        # new data slot
+        d <- data.frame(s[,1])
+        names(d) <- id
+
+        # recompose the object
+        res <- SpectraDataFrame(wl = wl(obj), nir = s[, -1], units = units(obj), data = d)
+      }
+      else
+        stop('Bad aggregation identifier.')
     }
 
     res
