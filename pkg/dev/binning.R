69d176b434f476b59f5f0f73740340efba02740b
roudierp <roudierp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-23 01:14:39 +0000

    implemented binning in dev, corrected CITATION file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inspectr@52 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dev/binning.R
+++ pkg/dev/binning.R
@@ -1,12 +1,33 @@
 ## Binning of the spectra
 
-# Create bins on WLs (or accept WLS as is?)
-# Affect each existing WL to a bin
-# For each spectra, for each bin, apply some kind of integrating fun (mean)
-# Reassemble spectra matrix
-# Update WLs (take central value?)
-# IDs, data,units unchanged
-# Throw the object back to the user
+bin_spectra <- function(object, bins = NULL, reduction = NULL, intervals = NULL, fun = mean, parallel = FALSE, ...) {
+  
+  if (is.null(intervals) & is.null(bins) & is.null(reduction))
+    stop("Please provide either wavelength intervals, compression factor or number of bins")
+  
+  if (length(which(laply(list(bins, reduction, intervals), is.null))) < 2)
+    stop("Please provide EITHER wavelength intervals, compression factor or number of bins")
 
-# r <- ddply(data.frame(group = as.numeric(cut(wl(australia), breaks = 250)), nir = spectra(australia)[1,]), .(group), function(x) median(x$nir))
+  if (!is.null(reduction))
+    bins <- ceiling(length(object)/reduction)
+  
+  # Create bins based on a number of bins (regular binning)
+  bins <- cut(wl(object), breaks = bins, labels = FALSE)
+  
+  # Affect each existing WL to a bin
+  mat <- data.frame(wl = wl(object), bins = bins, t(spectra(object)), check.names = FALSE)
+  mat$bins <- factor(mat$bins)
+  
+  levels(mat$bins) <- daply(mat, .(bins), function(x) ceiling(mean(x$wl)), .parallel = parallel)
 
+  mat$bins <- as.numeric(as.character(mat$bins))
+
+  agg_nir <- ddply(mat, .(bins), function(x) apply(x[,3:ncol(x)], 2, fun, ...), .parallel = parallel)
+  
+  nir <- t(agg_nir[, 2:ncol(agg_nir)])
+  colnames(nir) <- agg_nir$bins
+  
+  spectra(object) <- nir
+  
+  object
+}
