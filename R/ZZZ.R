844d50f84a9efff2964592033fa35ad7bc15382c
Pierre Roudier <pierre.roudier@gmail.com>
2018-01-16 11:23:27 +1300

    Added messages indicating the switch to spectacles

--- R/ZZZ.R
+++ R/ZZZ.R
@@ -1,6 +1,18 @@
 .onAttach <- function(lib, pkg)  {
     pkg.info <- drop(read.dcf(file=system.file("DESCRIPTION", package = pkg), fields = c("Version", "Date")))
-    packageStartupMessage(paste(pkg, " version ", pkg.info["Version"], " (", pkg.info["Date"], ")", sep = ""))
+    
+    packageStartupMessage(
+      '
+The inspectr package is now deprecated, and has been renamed as "spectacles". 
+      
+Please install the "spectacles" package from CRAN by running:
+
+> install.packages("spectacles")
+
+You can also visit the project page for more information at https://github.com/pierreroudier/spectacles
+
+      '
+    )
 
     return(invisible(0))
 }
